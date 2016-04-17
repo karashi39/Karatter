0656ffad5ca8447918efdfb8e1445d3ceb89709d
karashi39 <burrito.dr.peper@gmail.com>
2016-04-17 15:15:17 +0900

    integrated sql

--- login.sqlite.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS logins;
-CREATE TABLE logins (
-    id INTEGER PRIMARY KEY NOT NULL,
-    name text NOT NULL,
-    pass text NOT NULL,
-    created_at text NOT NULL,
-    updated_at text NOT NULL
-);
